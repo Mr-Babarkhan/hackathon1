e0d7ceb6796fddeeed8aee230c145ad7bb25c074
Mr-Babarkhan <muhammadbabarkhan813@gmail.com>
2023-08-23 23:01:45 -0700

    added slider

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -4,7 +4,7 @@ import AllProductsCompo from "@/components/AllProduct";
 async function fetchAllProductData() {
     let res = await fetch(`${BASE_PATH_FORAPI}/api/products?start=0&end=10`)
     if (!res.ok) {
-        throw new Error("Failed to fetch")
+        throw new Error("Failed to fetch Data ")
     }
     return res.json();
 };
